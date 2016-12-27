d4c84d6b242bb784dc6ecdbd4e8c28c331f9e3a4
carol <carol@DESKTOP-CCS6VFV>
2016-12-27 13:00:38 -0500

    Add project files.

---
+++ VidlyMovies/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyMovies.Startup))]
+namespace VidlyMovies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
